d50cfa1dfa02688b7f74bdf883f114a436a4943b
Andres Senac <andres@senac.es>
2024-01-01 10:44:19 +0100

    default_value_for_data_type

--- src/value.rs
+++ src/value.rs
@@ -1,5 +1,7 @@
 use core::fmt;
 
+use itertools::Itertools;
+
 use crate::data_type::DataType;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Hash, Debug)]
@@ -26,6 +28,24 @@ impl fmt::Display for Literal {
 }
 
 impl Literal {
+    pub fn new(value: Value, data_type: DataType) -> Self {
+        Self { value, data_type }
+    }
+
+    pub fn build_default(data_type: DataType) -> Self {
+        Self {
+            value: default_value_for_data_type(&data_type),
+            data_type,
+        }
+    }
+
+    pub fn build_null(data_type: DataType) -> Self {
+        Self {
+            value: Value::Null,
+            data_type,
+        }
+    }
+
     fn fmt(f: &mut fmt::Formatter, value: &Value, data_type: &DataType) -> fmt::Result {
         match (value, data_type) {
             (Value::Bool(value), DataType::Bool) => {
@@ -71,5 +91,23 @@ impl Literal {
         } else {
             false
         }
+    }
+}
+
+pub fn default_value_for_data_type(data_type: &DataType) -> Value {
+    match data_type {
+        DataType::Bool => Value::Bool(false),
+        DataType::Int => Value::Int(0),
+        DataType::BigInt => Value::BigInt(0),
+        DataType::String => Value::String("".to_string()),
+        DataType::Array(_) => Value::List(Vec::new()),
+        DataType::Tuple(members) => Value::List(
+            members
+                .iter()
+                .map(|nested_type| Box::new(default_value_for_data_type(nested_type)))
+                .collect_vec(),
+        ),
+        DataType::Any => Value::Null,
+        DataType::Unknown => panic!("cannot create value of unknown type"),
     }
 }
